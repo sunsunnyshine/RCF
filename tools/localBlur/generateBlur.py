e248ec615acfbc3d321e6aebac68be6d9b53c16b
sunsunnyshine <1741834627@qq.com>
2024-12-11 14:54:53 +0800

    12.11: syn on 1920*1280 VDV2 datasets num_classes = 3

--- tools/localBlur/generateBlur.py
+++ tools/localBlur/generateBlur.py
@@ -5,15 +5,15 @@ from tools.localBlur.blurtest import process_large_image
 
 def generateBlurFlow(gt_fw_flows, gt_bw_flows, fw_flow_agg, fw_residual_adjustment, bw_flow_agg,
                      bw_residual_adjustment, img, gt, origin_img, lookup_table, flow_range, title_size):
-    origin_H, origin_W = origin_img.shape[-2:]
 
-    ## step1: 统一到网络输入尺寸
-    resized_origin_img = F.interpolate(origin_img, size=img.shape[-2:], mode='bilinear')
-    gt_fw_flows = F.interpolate(gt_fw_flows[:, 0], size=img.shape[-2:], mode='bilinear')
-    gt_bw_flows = F.interpolate(gt_bw_flows[:, 0], size=img.shape[-2:], mode='bilinear')
-    fw_residual_adjustment = F.interpolate(fw_residual_adjustment, size=img.shape[-2:], mode='bilinear')
-    bw_residual_adjustment = F.interpolate(bw_residual_adjustment, size=img.shape[-2:], mode='bilinear')
-    gt = F.interpolate(gt.unsqueeze(0).float(), size=img.shape[-2:], mode='bilinear', align_corners=False).squeeze(0)
+    ## step1: 统一到原图尺寸
+    gt_fw_flows = F.interpolate(gt_fw_flows[:, 0], size=origin_img.shape[-2:], mode='bilinear')
+    gt_bw_flows = F.interpolate(gt_bw_flows[:, 0], size=origin_img.shape[-2:], mode='bilinear')
+    gt_fw_flows = gt_fw_flows * 2.0
+    gt_bw_flows = gt_bw_flows * 2.0
+    fw_residual_adjustment = F.interpolate(fw_residual_adjustment, size=origin_img.shape[-2:], mode='bilinear')
+    bw_residual_adjustment = F.interpolate(bw_residual_adjustment, size=origin_img.shape[-2:], mode='bilinear')
+    gt = F.interpolate(gt.unsqueeze(0).float(), size=origin_img.shape[-2:], mode='nearest').squeeze(0)
 
     ## step2： 生成模糊图像
     # 背景模糊，前景清晰
@@ -26,14 +26,11 @@ def generateBlurFlow(gt_fw_flows, gt_bw_flows, fw_flow_agg, fw_residual_adjustme
     bw_flow_blur = bw_flow_sharp + torch.mul(bw_residual_adjustment, gt[:, None, ...])
 
     # input: image[B,3,H,W], flow_forward[B,2,H,W], flow_backward[B,2,H,W], lookup_table[B,ker*ker,H,W], flow_range, tile_size
-    blur_image_objectsharp = process_large_image(resized_origin_img, fw_flow_sharp, bw_flow_sharp, lookup_table,
+    blur_image_objectsharp = process_large_image(origin_img, fw_flow_sharp, bw_flow_sharp, lookup_table,
                                                  flow_range, title_size)
     # output: numpy [B,C,H,W]
-    blur_image_objectblur = process_large_image(resized_origin_img, fw_flow_blur, bw_flow_blur, lookup_table,
+    blur_image_objectblur = process_large_image(origin_img, fw_flow_blur, bw_flow_blur, lookup_table,
                                                 flow_range, title_size)
 
-    ## step3: 统一到原图尺寸
-    blur_image_objectsharp = F.interpolate(blur_image_objectsharp, size=(origin_H, origin_W), mode='bilinear')
-    blur_image_objectblur = F.interpolate(blur_image_objectblur, size=(origin_H, origin_W), mode='bilinear')
 
     return blur_image_objectsharp, blur_image_objectblur
